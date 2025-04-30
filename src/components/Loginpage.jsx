ee82ca3d2ac501ace33ae647b21a3777b3ca627e
rushabhmane2004 <153699889+rushabhmane2004@users.noreply.github.com>
2025-05-01 00:06:19 +0530

    Updated files

--- src/components/Loginpage.jsx
+++ src/components/Loginpage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./loginpage.css";
+import "./Loginpage.css";
 
 export default function LoginPage({ setIsAuthenticated }) {
   const navigate = useNavigate();
